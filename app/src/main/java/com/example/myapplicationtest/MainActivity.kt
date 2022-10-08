69b4ed5563a0004f2ca44f46ae6938cb04d2e9f6
Sergej <vasiliokas@gmail.com>
2022-10-08 10:39:34 +0100

    TestApp - Kotlin init

---
+++ app/src/main/java/com/example/myapplicationtest/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.myapplicationtest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
